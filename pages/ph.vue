a10987a695dd4b7c7e03491c28a94daa08fe869f
personalleandrocesar <leandrocesar.goo@gmail.com>
2025-01-13 19:11:16 -0300

    updt

---
+++ pages/ph.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.username">
+        <img 
+          :src="`https://api.leandrocesar.com/uploads/${user.username}.${format}`" 
+          :alt="user.username"
+          width="150"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const users = ref([
+  { username: "leandrocosta" },
+  { username: "dmorais" },
+  { username: "" }
+]);
+
+const format = ref("png"); // Ajuste conforme o formato das imagens (jpg, png, etc.)
+</script>

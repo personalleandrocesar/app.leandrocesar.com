df7e59dac7d0212d85ffa230c9c3a16719c5f03f
personalleandrocesar <leandrocesar.goo@gmail.com>
2025-07-23 16:44:15 -0300

    updt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     '/api/**': { cors: true, headers: { 'access-control-allow-methods': 'GET, POST, DELETE, PUT' } },
     'https://api.leandrocesar.com/**': { cors: true, headers: { 'access-control-allow-methods': 'GET, POST, DELETE, PUT' } },
   },
+
   runtimeConfig: {
     apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
     public: {
       apiBase: '', // can be overridden by NUXT_PUBLIC_API_BASE environment variable
     }
   },
+
   app: {
     head: {
       charset: 'utf-8',
@@ -28,17 +31,22 @@ export default defineNuxtConfig({
       ]
     },
   },
+
   css: [
     '~/assets/main.css'
   ],
+
   modules: [
   '@nuxt/content',
   'nuxt-icon',
     '@vueuse/nuxt',
     '@nuxtjs/color-mode',
   ],
+
   // plugins: ['~/plugins/chart.js'],
   content: {
     // https://content.nuxtjs.org/api/configuration
-  }
+  },
+
+  compatibilityDate: '2025-07-23'
 })
